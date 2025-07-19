9c86f8c6adecf6c11ef012c9119fd34381ecd073
Joo <125026282+Joo-v7@users.noreply.github.com>
2025-07-19 14:05:54 +0900

    [Bronze V] Title: 이상한 기호, Time: 176 ms, Memory: 17756 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int plus = (n+m);
+        int minus = (n-m);
+        System.out.println(plus * minus);
+    }
+}
